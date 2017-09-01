902cbe6bc674ea1d30afa75b646b08a7c5debda5
zhongxunking <zhongxunking@163.com>
2017-09-01 18:10:15 +0800

    starter-jpa实现定制引导spring-data-jpa

---
+++ ant-boot-starters/ant-boot-starter-jpa/src/main/java/org/antframework/boot/jpa/boot/AntJpaRepositoriesAutoConfiguration.java
@@ -0,0 +1,39 @@
+/* 
+ * Copyright © 2017 www.lvmama.com
+ */
+
+/*
+ * 修订记录:
+ * @author 钟勋（zhongxun@lvmama.com） 2017-08-31 18:14 创建
+ */
+package org.antframework.boot.jpa.boot;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.JpaRepositoryConfigExtension;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean;
+
+import javax.sql.DataSource;
+
+/**
+ * jpa-repository自动配置类（导入相关配置类）
+ * <p>
+ * 由于spring-boot原生的集成spring-data-jpa没有提供扩展点，需要定制的话只能自己写引导，参考{@link JpaRepositoriesAutoConfiguration}
+ */
+@Configuration
+@ConditionalOnBean(DataSource.class)
+@ConditionalOnClass(JpaRepository.class)
+@ConditionalOnMissingBean({JpaRepositoryFactoryBean.class, JpaRepositoryConfigExtension.class})
+@AutoConfigureAfter(HibernateJpaAutoConfiguration.class)
+@AutoConfigureBefore(JpaRepositoriesAutoConfiguration.class)
+@Import(AntJpaRepositoriesConfigureRegistrar.class)
+public class AntJpaRepositoriesAutoConfiguration {
+}
---
+++ ant-boot-starters/ant-boot-starter-jpa/src/main/java/org/antframework/boot/jpa/boot/AntJpaRepositoriesConfigureRegistrar.java
@@ -0,0 +1,44 @@
+/* 
+ * Copyright © 2017 www.lvmama.com
+ */
+
+/*
+ * 修订记录:
+ * @author 钟勋（zhongxun@lvmama.com） 2017-08-31 18:12 创建
+ */
+package org.antframework.boot.jpa.boot;
+
+import org.antframework.boot.jpa.support.JpaQueryRepository;
+import org.springframework.boot.autoconfigure.data.AbstractRepositoryConfigurationSourceSupport;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.config.JpaRepositoryConfigExtension;
+import org.springframework.data.repository.config.RepositoryConfigurationExtension;
+
+import java.lang.annotation.Annotation;
+
+/**
+ * ant-jpa配置类（设置repository实现基础类）
+ * <p>
+ * 参考{@link org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfigureRegistrar}
+ */
+public class AntJpaRepositoriesConfigureRegistrar extends AbstractRepositoryConfigurationSourceSupport {
+
+    @Override
+    protected Class<? extends Annotation> getAnnotation() {
+        return EnableJpaRepositories.class;
+    }
+
+    @Override
+    protected Class<?> getConfiguration() {
+        return EnableAntJpaRepositoriesConfiguration.class;
+    }
+
+    @Override
+    protected RepositoryConfigurationExtension getRepositoryConfigurationExtension() {
+        return new JpaRepositoryConfigExtension();
+    }
+
+    @EnableJpaRepositories(repositoryBaseClass = JpaQueryRepository.class)
+    private static class EnableAntJpaRepositoriesConfiguration {
+    }
+}
