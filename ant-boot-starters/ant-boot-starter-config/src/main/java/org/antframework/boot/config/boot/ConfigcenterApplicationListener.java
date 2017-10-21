ebcc682e2bf4abdfdb461b4459105ceb2e8ce303
zhongxunking <zhongxunking@163.com>
2017-10-21 18:31:01 +0800

    config-starter：使用属性绑定工具类

--- ant-boot-starters/ant-boot-starter-config/src/main/java/org/antframework/boot/config/boot/ConfigcenterApplicationListener.java
+++ ant-boot-starters/ant-boot-starter-config/src/main/java/org/antframework/boot/config/boot/ConfigcenterApplicationListener.java
@@ -10,26 +10,18 @@ package org.antframework.boot.config.boot;
 
 import org.antframework.boot.config.ConfigContextHolder;
 import org.antframework.boot.core.Apps;
+import org.antframework.boot.core.Contexts;
 import org.antframework.configcenter.client.ConfigContext;
 import org.antframework.configcenter.client.spring.ConfigcenterPropertySource;
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertySource;
-import org.springframework.util.StringUtils;
 
 /**
- * 设置配置中心的应用监听器
+ * 配置中心应用监听器（将配置中心加入到environment）
  */
 public class ConfigcenterApplicationListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
-    /**
-     * 配置中心服务端地址
-     */
-    public static final String SERVER_URL_PROPERTY_NAME = "configcenter.serverUrl";
-    /**
-     * 配置中心使用的zookeeper地址（多个zookeeper以“,”分隔）
-     */
-    public static final String ZK_URLS_PROPERTY_NAME = "configcenter.zkUrls";
 
     @Override
     public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
@@ -44,22 +36,15 @@ public class ConfigcenterApplicationListener implements ApplicationListener<Appl
 
     // 构建初始化参数
     private ConfigContext.InitParams buildInitParams(ConfigurableEnvironment environment) {
-        String serverUrl = environment.getProperty(SERVER_URL_PROPERTY_NAME);
-        if (!StringUtils.hasText(serverUrl)) {
-            throw new IllegalArgumentException("未设置配置中心服务端地址：" + SERVER_URL_PROPERTY_NAME);
-        }
-        String[] zkUrls = StringUtils.commaDelimitedListToStringArray(environment.getProperty(ZK_URLS_PROPERTY_NAME));
-        if (zkUrls == null || zkUrls.length == 0) {
-            throw new IllegalArgumentException("未设置配置中心使用的zookeeper地址：" + ZK_URLS_PROPERTY_NAME);
-        }
+        ConfigcenterProperties properties = Contexts.buildProperties(ConfigcenterProperties.class);
 
         ConfigContext.InitParams initParams = new ConfigContext.InitParams();
         initParams.setAppCode(Apps.getAppCode());
         initParams.setQueriedAppCode(Apps.getAppCode());
         initParams.setProfileCode(environment.getActiveProfiles()[0]);
-        initParams.setServerUrl(serverUrl);
+        initParams.setServerUrl(properties.getServerUrl());
         initParams.setCacheFilePath(Apps.getConfigPath() + "/" + String.format("configcenter-%s.properties", environment.getActiveProfiles()[0]));
-        initParams.setZkUrls(zkUrls);
+        initParams.setZkUrls(properties.getZkUrls());
 
         return initParams;
     }
---
+++ ant-boot-starters/ant-boot-starter-config/src/main/java/org/antframework/boot/config/boot/ConfigcenterProperties.java
@@ -0,0 +1,47 @@
+/* 
+ * 作者：钟勋 (e-mail:zhongxunking@163.com)
+ */
+
+/*
+ * 修订记录:
+ * @author 钟勋 2017-10-21 18:06 创建
+ */
+package org.antframework.boot.config.boot;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * 配置中心属性
+ */
+@ConfigurationProperties(prefix = ConfigcenterProperties.PREFIX)
+public class ConfigcenterProperties {
+    /**
+     * 属性前缀
+     */
+    public static final String PREFIX = "configcenter";
+
+    // 配置中心服务端地址
+    @NotBlank
+    private String serverUrl;
+    // 配置中心使用的zookeeper地址（多个zookeeper以“,”分隔）
+    @NotEmpty
+    private String[] zkUrls;
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public String[] getZkUrls() {
+        return zkUrls;
+    }
+
+    public void setZkUrls(String[] zkUrls) {
+        this.zkUrls = zkUrls;
+    }
+}
