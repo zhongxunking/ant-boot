46953da20a802117643fc5325752bb80cd53be78
zhongxunking <zhongxunking@163.com>
2018-05-03 11:43:39 +0800

    starter-config：重构boot引导配置中心

---
+++ ant-boot-starters/ant-boot-starter-config/src/main/java/org/antframework/boot/config/ConfigContexts.java
@@ -0,0 +1,106 @@
+/* 
+ * 作者：钟勋 (e-mail:zhongxunking@163.com)
+ */
+
+/*
+ * 修订记录:
+ * @author 钟勋 2018-05-02 22:30 创建
+ */
+package org.antframework.boot.config;
+
+import org.antframework.boot.core.Apps;
+import org.antframework.boot.core.Contexts;
+import org.antframework.common.util.other.Cache;
+import org.antframework.configcenter.client.ConfigContext;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.io.File;
+import java.util.Set;
+
+/**
+ * 配置上下文操作类
+ */
+public class ConfigContexts {
+    // 配置上下文缓存
+    private static final Cache<String, ConfigContext> CACHE = new Cache<>(new Cache.Supplier<String, ConfigContext>() {
+        @Override
+        public ConfigContext get(String key) {
+            return buildConfigContext(key);
+        }
+    });
+
+    /**
+     * 获取配置上下文
+     *
+     * @param appCode 被查询配置的应用编码
+     */
+    public static ConfigContext get(String appCode) {
+        return CACHE.get(appCode);
+    }
+
+    /**
+     * 获取已缓存的应用编码
+     */
+    public static Set<String> getAppCodes() {
+        return CACHE.getAllKeys();
+    }
+
+    // 构建配置上下文
+    private static ConfigContext buildConfigContext(String queriedAppCode) {
+        ConfigcenterProperties properties = Contexts.buildProperties(ConfigcenterProperties.class);
+
+        ConfigContext.InitParams initParams = new ConfigContext.InitParams();
+        initParams.setAppCode(Apps.getAppCode());
+        initParams.setQueriedAppCode(queriedAppCode);
+        initParams.setProfileCode(Contexts.getProfile());
+        initParams.setServerUrl(properties.getServerUrl());
+        initParams.setCacheFilePath(Apps.getConfigPath() + File.separator + String.format("configcenter-%s-%s.properties", queriedAppCode, Contexts.getProfile()));
+        initParams.setZkUrls(properties.getZkUrls().toArray(new String[0]));
+
+        return new ConfigContext(initParams);
+    }
+
+    /**
+     * 配置中心属性
+     */
+    @ConfigurationProperties(prefix = ConfigcenterProperties.PREFIX)
+    public class ConfigcenterProperties {
+        /**
+         * 属性前缀
+         */
+        public static final String PREFIX = "configcenter";
+        /**
+         * 是否开启监听的属性名
+         */
+        public static final String LISTEN_ENABLE_PROPERTY_NAME = PREFIX + ".listen.enable";
+
+        /**
+         * 必填：配置中心服务端地址（比如：http://localhost:6220）
+         */
+        @NotBlank
+        private String serverUrl;
+        /**
+         * 必填：配置中心使用的zookeeper地址，存在多个zookeeper的话以“,”分隔（比如：192.168.0.1:2181,192.168.0.2:2181）
+         */
+        @NotEmpty
+        private Set<String> zkUrls;
+
+        public String getServerUrl() {
+            return serverUrl;
+        }
+
+        public void setServerUrl(String serverUrl) {
+            this.serverUrl = serverUrl;
+        }
+
+        public Set<String> getZkUrls() {
+            return zkUrls;
+        }
+
+        public void setZkUrls(Set<String> zkUrls) {
+            this.zkUrls = zkUrls;
+        }
+    }
+}
--- ant-boot-starters/ant-boot-starter-config/src/main/java/org/antframework/boot/config/boot/ConfigcenterApplicationListener.java
+++ ant-boot-starters/ant-boot-starter-config/src/main/java/org/antframework/boot/config/boot/ConfigcenterApplicationListener.java
@@ -4,58 +4,59 @@
 
 /*
  * 修订记录:
- * @author 钟勋 2017-09-25 21:25 创建
+ * @author 钟勋 2018-05-03 10:10 创建
  */
 package org.antframework.boot.config.boot;
 
-import org.antframework.boot.config.ConfigContextHolder;
+import org.antframework.boot.config.ConfigContexts;
 import org.antframework.boot.core.Apps;
-import org.antframework.boot.core.Contexts;
 import org.antframework.configcenter.client.ConfigContext;
-import org.antframework.configcenter.client.spring.ConfigcenterPropertySource;
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
 import org.springframework.boot.logging.LoggingApplicationListener;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.annotation.Order;
-import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
 import org.springframework.core.env.PropertySource;
-
-import java.io.File;
 
 /**
  * 配置中心应用监听器（将配置中心加入到environment）
  */
 @Order(LoggingApplicationListener.DEFAULT_ORDER + 1)
 public class ConfigcenterApplicationListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
-
     @Override
     public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
-        ConfigurableEnvironment environment = event.getEnvironment();
-        // 构造配置上下文
-        ConfigContext configContext = new ConfigContext(buildInitParams());
-        // 将配置中心设置到environment中
-        environment.getPropertySources().addLast(buildPropertySource(configContext));
-        // 初始化配置上下文持有器
-        ConfigContextHolder.init(configContext);
+        // 创建配置上下文属性资源
+        PropertySource propertySource = new ConfigContextPropertySource(ConfigContexts.get(Apps.getAppCode()));
+        // 将属性资源添加到environment中
+        event.getEnvironment().getPropertySources().addLast(propertySource);
     }
 
-    // 构建初始化参数
-    private ConfigContext.InitParams buildInitParams() {
-        ConfigcenterProperties properties = Contexts.buildProperties(ConfigcenterProperties.class);
+    /**
+     * 配置上下文属性资源
+     */
+    public static class ConfigContextPropertySource extends EnumerablePropertySource<ConfigContext> {
+        /**
+         * 属性资源名称
+         */
+        public static final String PROPERTY_SOURCE_NAME = "configcenter";
 
-        ConfigContext.InitParams initParams = new ConfigContext.InitParams();
-        initParams.setAppCode(Apps.getAppCode());
-        initParams.setQueriedAppCode(Apps.getAppCode());
-        initParams.setProfileCode(Contexts.getProfile());
-        initParams.setServerUrl(properties.getServerUrl());
-        initParams.setCacheFilePath(Apps.getConfigPath() + File.separator + String.format("configcenter-%s.properties", Contexts.getProfile()));
-        initParams.setZkUrls(properties.getZkUrls().toArray(new String[0]));
+        public ConfigContextPropertySource(ConfigContext source) {
+            super(PROPERTY_SOURCE_NAME, source);
+        }
 
-        return initParams;
-    }
+        @Override
+        public boolean containsProperty(String name) {
+            return source.getProperties().contains(name);
+        }
 
-    // 构建配置中心在environment中的属性资源
-    private PropertySource buildPropertySource(ConfigContext configContext) {
-        return new ConfigcenterPropertySource(ConfigcenterPropertySource.PROPERTY_SOURCE_NAME, configContext);
+        @Override
+        public String[] getPropertyNames() {
+            return source.getProperties().getPropertyKeys();
+        }
+
+        @Override
+        public Object getProperty(String name) {
+            return source.getProperties().getProperty(name);
+        }
     }
 }
