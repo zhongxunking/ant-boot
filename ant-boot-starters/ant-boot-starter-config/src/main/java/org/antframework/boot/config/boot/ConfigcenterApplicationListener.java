f3f810f75eb5c4586bd6999c36421ef00b7b39a5
zhongxunking <zhongxunking@163.com>
2019-02-24 01:58:00 +0800

    starter-config：适配configcenter变更

--- ant-boot-starters/ant-boot-starter-config/src/main/java/org/antframework/boot/config/boot/ConfigcenterApplicationListener.java
+++ ant-boot-starters/ant-boot-starter-config/src/main/java/org/antframework/boot/config/boot/ConfigcenterApplicationListener.java
@@ -14,19 +14,21 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
+import java.io.File;
+
 /**
  * 配置中心应用监听器
  */
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ConfigcenterApplicationListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
-    // 配置中心使用的应用id属性名
-    private static final String APP_ID_PROPERTY_NAME = "configcenter.app-id";
-    // 配置中心缓存目录属性名
-    private static final String CACHE_DIR_PROPERTY_NAME = "configcenter.cache-dir";
+    // 配置中心使用的应用id--key
+    private static final String APP_ID_KEY = "configcenter.app-id";
+    // 配置中心缓存目录key
+    private static final String CACHE_DIR_KEY = "configcenter.cache-dir-path";
 
     @Override
     public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
-        System.setProperty(APP_ID_PROPERTY_NAME, Apps.getAppId());
-        System.setProperty(CACHE_DIR_PROPERTY_NAME, Apps.getConfigPath());
+        System.setProperty(APP_ID_KEY, Apps.getAppId());
+        System.setProperty(CACHE_DIR_KEY, Apps.getConfigPath() + File.separator + "configcenter");
     }
 }
